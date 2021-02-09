226dfdd7dc9478e86aa163e226891e88287ddacd
MarcoB <mrc.bsllt@gmail.com>
2021-02-09 15:08:02 +0100

    aggiunto in pagina le macchine prese dal database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,18 @@
     @include("template/header");
 
     <main>
+
       <div class="cars">
-
-        <div class="car"></div>
-
+        @foreach ($cars as $car)
+          <div class="car">
+            <img src="https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg" alt="foto">
+            <h2>{{ $car->brand }} - {{ $car->model }}</h2>
+            <p>Numero di targa: <strong>{{ $car->number_plate }}</strong></p>
+          </div>
+        @endforeach
       </div>
+
     </main>
-    
+
   </body>
 </html>
