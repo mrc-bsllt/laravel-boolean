1d38f31b9691714a4ce94779608b74afcf2a19c9
MarcoB <mrc.bsllt@gmail.com>
2021-02-09 14:10:15 +0100

    creato il controller per la home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home Cars</title>
+  </head>
+  <body>
+    <h1>Ciao come stai?</h1>
+  </body>
+</html>
