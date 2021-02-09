71d9bb10510bf9a2658d50fb737538c69cb77b54
MarcoB <mrc.bsllt@gmail.com>
2021-02-09 16:36:48 +0100

    cambiata l'annotazione per richiamare un valore, tolta la frecca pe rle parentesi quadre

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
   <main>
-    <h2>{{ $car->brand }}-{{ $car->model }}</h2>
-    <h3>Targa: <small>{{ $car->number_plate }}</small></h3>
+    <h2>{{ $car["brand"] }}-{{ $car["model"] }}</h2>
+    <h3>Targa: <small>{{ $car["number_plate"] }}</small></h3>
   </main>
 @endsection
